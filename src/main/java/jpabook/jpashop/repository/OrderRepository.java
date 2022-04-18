2254d821fbd3fe3df6aab9a20ae206d0ecd02f5f
Hyeongki-Kim01 <sswwx1@naver.com>
2022-04-18 18:07:07 +0900

    주문 검색 기능 개발 3:48

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,7 +21,12 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+
+
+        em.createQuery("select o from Order o join o.member m" +
+                        " where ",
+                Order.class);
+    }
 }
