a935907d6b437e127a5602204cca420a056be5c3
kh-kim <sswwx1@naver.com>
2022-05-18 17:25:48 +0900

    상속관계 매핑 06:00

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,8 +3,13 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED) // 기본은 단일테이블 전략, joined == join 전략
-public class Item {
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 기본은 단일테이블 전략
+
+//@Inheritance(strategy = InheritanceType.JOINED) // 기본은 단일테이블 전략, joined == join 전략
+//@DiscriminatorColumn // default -> entity명 (단일테이블 전략일때 필요없음-자동세팅됨) (join 전략일때 필요)
+
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 구현 클래스 마다 테이블 전략 (
+public abstract class Item { // 구현 클래스 마다 테이블 전략(abstract class) 나머지 (class) // 쓰지않는게 좋다
 
     @Id @GeneratedValue
     private Long id;
