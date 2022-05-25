d2f133e648cd3c095d2ed12c68942dd75518d515
kh-kim <sswwx1@naver.com>
2022-05-25 16:58:15 +0900

    요구사항 분석

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest // 스프링 컨테이너 안에서 테스트 가능 (@Autowired .. 등등 스프링 기능 사용 가능)
 public class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
@@ -31,5 +31,9 @@ public class MemberRepositoryTest {
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getId()).isEqualTo(saveId);
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member : " + (findMember == member)); // 영속성 컨텍스트에서 같은 식별자이기 때문에 (1차 캐시)
+
     }
 }
