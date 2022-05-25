e1024d5691ebd26f191981b5865c8b2c3d87626d
aaqqz <sswwx1@naver.com>
2022-05-26 01:32:29 +0900

    엔티티 클래스 개발1 09:00

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+}
