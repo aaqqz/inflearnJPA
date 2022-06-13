efeab0527ece9365cfbcc667fafef7114fe5aa35
aaqqz <sswwx1@naver.com>
2022-06-13 09:34:49 +0900

    Web 확장 - 페이징과 정렬

--- src/test/java/study/datajpa/entity/MemberTest.java
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,14 +1,17 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +22,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() throws Exception {
@@ -50,5 +56,27 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("-> member.test = " + member.getTeam());
         }
+    }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member); // @PrePersist
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+        
+        em.flush(); // @PreUpdate
+        em.clear();
+        
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("findMember.createDate = " + findMember.getCreateDate());
+        System.out.println("findMember.createBy = " + findMember.getCreateBy());
+        System.out.println("findMember.updateDate = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.updateBy = " + findMember.getLastModifiedBy());
     }
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -36,6 +36,9 @@ class MemberRepositoryTest {
     @PersistenceContext
     EntityManager em;
 
+    @Autowired
+    MemberQueryRepository memberQueryRepository;
+
     @Test
     public void testMember() throws Exception {
         System.out.println("memberRepository = " + memberRepository.getClass());
@@ -288,5 +291,38 @@ class MemberRepositoryTest {
         List<Member> findMemberEntityGraph = memberRepository.findMemberEntityGraph();
         List<Member> findEntityGraphByUsername = memberRepository.findEntityGraphByUsername("member1");
 
+    }
+
+    @Test
+    public void queryHint(){
+        // given
+        Member member1 =new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        // when
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2");
+        em.flush();
+    }
+
+    @Test
+    public void lock(){
+        // given
+        Member member1 =new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> result = memberRepository.findLockByUsername("member1");
+    }
+
+    @Test
+    public void callCustom() {
+        Member member1 =new Member("member1", 10);
+        memberRepository.save(member1);
+        List<Member> result = memberRepository.findMemberCustom();
     }
 }
