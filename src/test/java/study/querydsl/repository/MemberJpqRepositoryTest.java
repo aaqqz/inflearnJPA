df75a683c241ee1a2434b1615200f1b614dfa47e
kh-kim <sswwx1@naver.com>
2023-02-06 11:23:16 +0900

    동적쿼리 성능 최적화 - where 파라미터 사용

--- src/test/java/study/querydsl/repository/MemberJpqRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpqRepositoryTest.java
@@ -1,12 +1,16 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.annotations.QueryProjection;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 
@@ -54,5 +58,67 @@ class MemberJpqRepositoryTest {
 
         List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    @DisplayName("동적 쿼리와 성능 최적화 조회 - Builder 사용")
+    void searchTest() {
+        // MemberTeamDto - 조회 최적화용 DTO 추가 (성능 최적화)
+        // booleanBuilder 사용 동적 쿼리 사용
+
+//        @QueryProjection 을 사용하면 해당 DTO가 Querydsl을 의존하게 된다. 이런 의존이 싫으면,
+//        해당 에노테이션을 제거하고, Projection.bean(), fields(), constructor() 을 사용하면 된다
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+        assertThat(result).extracting("username").containsExactly("member4");
+    }
+
+    @Test
+    @DisplayName("동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용(실무에서 자주 사용)")
+    void searchTest2() {
+        // 실무에서 자주 사용 (Builder 보다 Where절 파라미터 사용 권장)
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
+
+        assertThat(result).extracting("username").containsExactly("member4");
     }
 }
